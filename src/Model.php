79743a1eb3c82a473c5908bd768b8465f5830a8e
Chandler Van Scoy <chandlerv@bioreigns.com>
2020-04-12 02:49:35 -0700

    Quick save function in model class

--- src/Model.php
+++ src/Model.php
@@ -2,10 +2,18 @@
 declare(strict_types=1);
 
 namespace DataHead\InterfazFramework;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping as ORM;
 
 class Model
 {
+    /** @var EntityManager $entityManager */
+    protected $entityManager;
+    public function __construct()
+    {
+        $this->entityManager = Framework::getInstance()->container->get(EntityManager::class);
+    }
+
     /**
      * @var int
      * @ORM\Id
@@ -20,5 +28,10 @@ class Model
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function save() {
+        $this->entityManager->persist($this);
+        $this->entityManager->flush();
     }
 }
